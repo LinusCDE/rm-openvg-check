fac6722b5a61fe5332752048d4f27bab1046ed85
Linus <linus@cosmos-ink.net>
2021-12-03 17:18:41 +0100

    Test

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
-use egli::Display;
+use khronos_egl as egl;
 
-fn main() -> egli::error::Result<()> {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let instance = egl::Instance::new(egl::Static);
+
+    println!("Version: {}", instance.version());
+    /*
     println!("Extensions:");
-    for ext in egli::query_extensions() {
+    for ext in egl::query_extensions() {
         if ext.contains(" ") {
             for ext in ext.split(" ") {
                 println!(" - {}", ext);
@@ -17,20 +21,7 @@ fn main() -> egli::error::Result<()> {
     println!("Opened display");
     display.initialize()?;
     println!("Initialized display");
-    println!("Client APIs:");
-    for api in display.query_client_apis() {
-        if api.contains(" ") {
-            for api in api.split(" ") {
-                println!(" - {}", api);
-            }
-        } else {
-            println!(" - {}", api);
-        }
-    }
 
-    println!("Vendor: {}", display.query_vendor()?);
-    println!("Version: {}", display.query_version()?);
-
-    display.create_context
+    display.create_context*/
     Ok(())
 }
